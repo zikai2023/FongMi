62082a7507fb68c1b118b651a0f1a6cddd23abba
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-03-21 12:11:20 +0800

    Add files via upload
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/leanback/java/com/fongmi/android/tv/Updater.java
+++ app/src/leanback/java/com/fongmi/android/tv/Updater.java
@@ -82,13 +82,7 @@ public class Updater implements Download.Callback {
             String name = object.optString("name");
             String desc = object.optString("desc");
             int code = object.optInt("code");
-            if (need(code, name)) App.post(() -> show(activity, name, desc));}
-else {
-Handler handler = new Handler(Looper.getMainLooper()); new
-handler.post(new Runnable() {
-new
-@Override
-public void run() { Notify.show(text:"已是最新版本});
+            if (need(code, name)) App.post(() -> show(activity, name, desc));
         } catch (Exception e) {
             e.printStackTrace();
         }
