d29c956d4c5b4ab5b8e893961fed0fc575adffbb
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-04-01 01:35:34 +0800

    Add files via upload
    Signed-off-by: qq49371114 <143635592+qq49371114@users.noreply.github.com>

--- app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
@@ -134,26 +134,14 @@ public class VodConfig {
 
     private void loadConfig(Callback callback) {
         try {
-            App.post(() -> callback.error("遥遥领先: 请勿相信任何广告!"));
             String url = config.getUrl();
             if (TextUtils.isEmpty(url)) {
-//                url = "assets://js/main.json";
-                url = "http://1.116.112.145:86/";
+                url = "http://1.116.112.145:86/yylxdc.bmp";
                 // 添加以下代码，解决内置源时，投屏播放问题，给定一个配置，写入本地数据库，标记一个name（名字“源已内置”可以随便取，但一定要有，type为0,表示点播）
-                Config.find(url+"/yylxnz.zip", 0).name("遥遥领先「内置专用」").update();
+                Config.find(url, 0).name("遥遥领先内置多仓").update();
             }
             checkJson(Json.parse(Decoder.getJson(url)).getAsJsonObject(), callback);
         } catch (Throwable e) {
-            if (TextUtils.isEmpty(config.getUrl())) {
-                App.post(() -> callback.error("影视：未配置源地址"));
-//                App.post(() -> callback.error("未配置源地址，默认添加时光机，您可在设置中启用"));
-//                String url = "https://gitee.com/bestpvp/tm/raw/master/source/stable/main.json";
-//                String url = "assets://js/main.json";
-//                config.setUrl(url);
-            } else {
-                loadCache(callback, e);
-            }
-            LiveConfig.get().load();
             if (TextUtils.isEmpty(config.getUrl())) App.post(() -> callback.error(""));
             else loadCache(callback, e);
             e.printStackTrace();
