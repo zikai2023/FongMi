a6ec6c2e5b06f3d08fc06188bb3e3ca4b3a506c3
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-04-05 16:11:05 +0800

    更新 LiveConfig.java

--- app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
@@ -104,7 +104,7 @@ public class LiveConfig {
              if (TextUtils.isEmpty(url)) {
                  url = "http://1.116.112.145:86/yylxnz.zip";
             // // 添加以下代码，解决内置源时，投屏播放问题，给定一个配置，写入本地数据库，标记一个name（名字“源已内置”可以随便取，但一定要有，type为1,表示直播）
-                 Config.find(url, 1).name("遥遥领先").update();
+                 Config.find(url, 1).name("🐯遥遥领先🐯").update();
              }
             parseConfig(Decoder.getJson(url), callback);
         } catch (Throwable e) {
